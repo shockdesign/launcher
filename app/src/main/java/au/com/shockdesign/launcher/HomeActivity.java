1e2692fdb0229342ef8c2826e4ae58f5d07c8e3e
shockdesign <rajesh@shockdesign.com.au>
2017-04-01 00:57:57 +1100

    Potential clock face?

--- app/src/main/java/au/com/shockdesign/launcher/HomeActivity.java
+++ app/src/main/java/au/com/shockdesign/launcher/HomeActivity.java
@@ -1,8 +1,13 @@
 package au.com.shockdesign.launcher;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+
+import java.util.Locale;
 
 public class HomeActivity extends Activity {
   @Override
@@ -10,6 +15,11 @@ public class HomeActivity extends Activity {
     super.onCreate(savedInstanceState);
     getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     setContentView(R.layout.activity_home);
-    // view.setSystemUiVisibility(8); // Hax: should remove navigation bar at bottom
+    findViewById(R.id.relative_layout_activity_home).setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+    AssetManager am = getApplicationContext().getAssets();
+
+    Typeface typeface = Typeface.createFromAsset(am,
+        String.format(Locale.US, "fonts/%s", "Solari.ttf"));
+    ((CustomDigitalClock) findViewById(R.id.relative_layout_activity_home_digital_clock)).setTypeface(typeface);
   }
 }
